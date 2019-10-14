0d984fc70a50341dbf7bc81cdeccbacb4c4e3084
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-14 02:42:40 +0200

    Korriger prefiks for livsmedium

--- stages/transform/30_original.js
+++ stages/transform/30_original.js
@@ -12,6 +12,28 @@ io.skrivBuildfil("art", items);
 function reorganize(e) {
   Object.keys(e).forEach(k => stripHtml(e, k));
   Object.keys(structure).forEach(key => json.moveKey(e, key, structure[key]));
+  livsmedium(e);
+}
+
+function livsmedium(e) {
+  const rv = e.risikovurdering;
+  if (!rv) return;
+  const kriterie = rv.kriterie;
+  if (!kriterie) return;
+  livsmedium2(kriterie.c, "koloniserte naturtyper");
+  livsmedium2(kriterie.e, "naturtyper");
+}
+
+function livsmedium2(kriterie, key) {
+  if (!kriterie) return;
+  const typer = kriterie[key];
+  if (!typer) return;
+  kriterie[key] = typer.map(k => {
+    const livsmedium_hoved = ["MS", "MF", "FS", "FF", "TS", "TF"];
+    if (k.indexOf("LI ") === 0) return k.replace("LI ", "LI-");
+    for (var li of livsmedium_hoved) if (k.indexOf(li) === 0) return "LI-" + k; //Livsmedium
+    return "NN-NA-TI-" + k;
+  });
 }
 
 function stripHtml(e, k) {
